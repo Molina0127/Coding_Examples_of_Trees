854a70e4a3ca4684a40058a367b999d22962436b
Guilherme Molina Trindade <78188699+Molina0127@users.noreply.github.com>
2021-02-16 08:42:06 -0300

    Primeiro commit

---
+++ src/arvores_binarias/Elemento.java
@@ -0,0 +1,41 @@
+package arvores_binarias;
+
+public class Elemento <nodo>{
+
+	private nodo valor;
+	private Elemento<nodo> esquerda;
+	private Elemento<nodo> direita;
+	
+	public Elemento(nodo novoValor) {
+		this.valor = novoValor;
+		this.esquerda = null;
+		this.direita = null;
+	}
+
+	public nodo getValor() {
+		return valor;
+	}
+
+	public void setValor(nodo valor) {
+		this.valor = valor;
+	}
+
+	public Elemento<nodo> getEsquerda() {
+		return esquerda;
+	}
+
+	public void setEsquerda(Elemento<nodo> esquerda) {
+		this.esquerda = esquerda;
+	}
+
+	public Elemento<nodo> getDireita() {
+		return direita;
+	}
+
+	public void setDireita(Elemento<nodo> direita) {
+		this.direita = direita;
+	}
+	
+	
+	
+}
